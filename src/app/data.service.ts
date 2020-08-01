4208721f95dc8b0353ff916242531b48f0fafcbc
Devaarth <devaarth@gmail.com>
2020-08-01 21:57:23 +0530

    Initial commit for ng2 search filter

---
+++ src/app/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable()
+export class DataService {
+
+  public vegetableSubject$ = new Subject<any>();
+  constructor() { 
+  }
+
+  public setVegetableList(vegetable: any) {
+    if (vegetable) {
+      this.vegetableSubject$.next(vegetable);
+    }
+   
+  }
+ 
+  public getVegetable(): Observable<any> {
+    return this.vegetableSubject$.asObservable();
+  }
+}
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DataService } from '../data.service';
+@Component({
+  selector: 'search-component',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+//using normal data passing instead of Reactive Form
+  public inputVal: any;
+  constructor( public service: DataService) { 
+    
+  }
+
+  ngOnInit() {
+    // add corresponding validators
+  
+
+     // write a function that calls changeVegetableName upon value change in the form
+  }
+  onFormChange($event) {
+   // console.log($event);
+    this.service.setVegetableList($event);
+  }
+
+}
